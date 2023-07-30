4f444af9450f5cbda6aae4f8176711e0bd34a8ff
Eliran Shemesh <piro27@gmail.com>
2023-07-30 19:48:47 +0300

    Fix type

--- utils/posts.ts
+++ utils/posts.ts
@@ -1,6 +1,6 @@
 const AVERAGE_HUMAN_READING_SPEED = 190;
 
-export const calculateReadingTime = (text) => {
+export const calculateReadingTime = (text: string) => {
    const words = text.split(" ");
    const minutes = words.length / AVERAGE_HUMAN_READING_SPEED;
    return Math.round(minutes);
