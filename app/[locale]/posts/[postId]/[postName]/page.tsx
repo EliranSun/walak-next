2a3dcfafdd7b67e89318932a5cfe5e424548fcb6
Eliran Shemesh <piro27@gmail.com>
2023-08-06 18:20:46 +0300

    Post dynamic metadata + move supabase to util + implement authors

--- app/[locale]/posts/[postId]/[postName]/page.tsx
+++ app/[locale]/posts/[postId]/[postName]/page.tsx
@@ -1,15 +1,25 @@
-import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
-import {cookies} from "next/headers";
-import {calculateReadingTime, getPost} from "@/utils/posts";
+import React from "react";
+import {Metadata} from 'next';
+import {trackPostViews, getPost} from "@/utils/posts";
 import {Tags} from "@/components/molecules/Tags";
 import Author from "@/components/molecules/Author";
 import {PostFooterMessage} from "@/components/atoms/PostFooterMessage";
 import {PrintViewButton} from "@/components/molecules/PrintViewButton";
 import {ShareButton} from "@/components/molecules/ShareButton";
 import {PostContent} from "@/components/organisms/PostContent";
+import Authors from "@/components/molecules/Authors";
+
+export async function generateMetadata(
+   {params}: { params: { postId: string, postName: string } },
+): Promise<Metadata> {
+   const postName = decodeURI(params.postName).replace(/-/g, ' ');
+
+   return {
+      title: `וואלק | ${postName}`
+   }
+}
 
 export default async function Index({params}: { params: { postId: string, postName: string } }) {
-   const supabase = createServerComponentClient({cookies})
    const {post, error} = await getPost(Number(params.postId));
 
    if (error || !post) {
@@ -22,63 +32,51 @@ export default async function Index({params}: { params: { postId: string, postNa
       );
    }
 
-   supabase
-      .from('postsViews')
-      .select()
-      .eq('postId', post.id)
-      .then(({data}) => {
-         supabase
-            .from('postsViews')
-            .upsert({
-               count: (data && data[0]?.count || 0) + 1,
-               postId: post.id
-            })
-            .eq('postId', post.id)
-            .then(({data, error}) => {
-               if (error) {
-                  console.log(error);
-               }
-            })
-      });
-
-   console.log(post.authors);
+   trackPostViews(post);
 
    return (
-      <div className="w-full flex flex-col items-center post" dir="rtl">
-         <div
-            className="w-full h-[66vh] overflow-hidden relative bg-cover bg-fixed bg-[center_bottom_5rem] flex items-end justify-center"
-            style={{
-               backgroundImage: `url(${post.upscaledImageSrc || post.imageSrc})`
-            }}>
-            <div className="w-full bg-yellow-500 text-center py-8 text-white z-10">
-               <h1 className="text-6xl open-sans tracking-wider">{post.title}</h1>
-               <h2 className="text-xl open-sans">{post.excerpt}</h2>
-            </div>
-         </div>
-         <div className="flex items-start my-8 justify-center">
-            <div className="w-1/6 px-4 flex justify-end flex flex-col box-border items-center">
-               {post.authors.map(({author}: { author: Author }) => {
-                  return <Author author={author} post={post}/>;
-               })}
-            </div>
-            <PostContent post={post}/>
-            <div className="flex flex-wrap w-1/6 m-4 px-4 gap-2">
-               <Tags tags={post.tags || []} isInteractive={post.isInteractive} hasVideo={post.hasVideo}/>
-               <div className="border-b border-dotted border-gray-400 w-full h-1"/>
-               <div className="flex flex-col gap-2 my-2">
-                  <PrintViewButton/>
-                  <ShareButton/>
+      <>
+         <div className="w-full flex flex-col items-center post" dir="rtl">
+            <div
+               className="w-full h-[70vh] overflow-hidden relative bg-cover bg-fixed bg-[center_bottom_5rem] flex items-end justify-center"
+               style={{
+                  backgroundImage: `url(${post.upscaledImageSrc || post.imageSrc})`
+               }}>
+               <div className="w-full bg-yellow-500 text-center py-8 text-white z-10">
+                  <h1 className="text-6xl open-sans tracking-wider">{post.title}</h1>
+                  <h2 className="text-xl open-sans">{post.excerpt}</h2>
                </div>
-               <div className="border-b border-dotted border-gray-400 w-full h-1"/>
-               {/*<img*/}
-               {/*    className="mt-10 w-40"*/}
-               {/*    alt="Advertisement"*/}
-               {/*    src="https://d2r55xnwy6nx47.cloudfront.net/uploads/2021/09/2021PodcastAd_Article_160.jpg"*/}
-               {/*/>*/}
             </div>
+            <div className="flex items-start my-8 justify-center">
+               {/*<div className="flex flex-wrap w-1/6 m-4 px-4 gap-2">*/}
+
+               {/*</div>*/}
+               <PostContent post={post}/>
+               <div className="w-44 mr-8 flex justify-end flex gap-2 flex-col box-border items-center">
+                  {post.authors.length > 1
+                     ? <Authors authors={post.authors}/>
+                     : <Author author={post.authors[0]}/>}
+                  {post.createdAt && (
+                     <div className="border-b border-gray-300 w-full pb-2 opacity-60 text-xs">
+                        {new Date(post.createdAt).toLocaleString("he-IL", {
+                           weekday: "long",
+                           year: "numeric",
+                           month: "long",
+                           day: "numeric",
+                        })}
+                     </div>
+                  )}
+                  <Tags tags={post.tags || []} isInteractive={post.isInteractive} hasVideo={post.hasVideo}/>
+                  <div className="border-b border-dotted border-gray-400 w-full h-1"/>
+                  <div className="w-full flex flex-col gap-2">
+                     <PrintViewButton/>
+                     <ShareButton/>
+                  </div>
+               </div>
+            </div>
+            <hr/>
+            <PostFooterMessage/>
          </div>
-         <hr/>
-         <PostFooterMessage/>
-      </div>
+      </>
    );
 };
