1a4328a48f2fed2cf86436591790ab35f683f6c7
Eliran Shemesh <piro27@gmail.com>
2023-08-13 15:23:38 +0300

    Original optimized photo through cloudinary

--- app/[locale]/posts/[postId]/[postName]/page.tsx
+++ app/[locale]/posts/[postId]/[postName]/page.tsx
@@ -44,8 +44,7 @@ export default async function Index({params}: { params: { postId: string, postNa
             <meta property="description" content={post.excerpt}/>
             <meta property="og:title" content={post.title}/>
             <meta property="og:description" content={post.excerpt}/>
-            <meta property="og:image"
-                  content="https://res.cloudinary.com/deqnpyqu3/image/upload/v1691926761/ixfhwn7vb2ktfqwwdotr.jpg"/>
+            <meta property="og:image" content={post.imageSrc}/>
             <meta property="og:image:width" content="1200"/>
             <meta property="og:image:height" content="666"/>
             <meta property="og:image:type" content="image/jpeg"/>
@@ -56,8 +55,7 @@ export default async function Index({params}: { params: { postId: string, postNa
             <meta property="twitter:site" content="@Walak"/>
             <meta property="twitter:creator" content="@Walak"/>
             <meta property="twitter:card" content="summary_large_image"/>
-            <meta name="twitter:image"
-                  content="https://res.cloudinary.com/deqnpyqu3/image/upload/v1691926761/ixfhwn7vb2ktfqwwdotr.jpg"/>
+            <meta name="twitter:image" content={post.imageSrc}/>
             <meta name="twitter:label1" content="Written by"/>
             <meta name="twitter:data1" content="Eliran Shemesh & Ofir Cohen"/>
             <meta name="twitter:label2" content="Est. reading time"/>
