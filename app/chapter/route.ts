ad28fd677945f42b7ba2860cdd2bc93915ea6966
Eliran Shemesh <piro27@gmail.com>
2023-12-07 19:12:46 +0200

    Handle invalid request

--- app/chapter/route.ts
+++ app/chapter/route.ts
@@ -2,7 +2,7 @@ import {NextRequest, NextResponse} from "next/server";
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-   apiKey: "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",
+    apiKey: "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",
 });
 
 const firstChapterPrompt = (title: string, siblingName: string, siblingType: string) => `
@@ -15,14 +15,14 @@ const firstChapterPrompt = (title: string, siblingName: string, siblingType: str
    Output nothing but the story chapter!`;
 
 const nthChapterPrompt = ({
-   title, chapterNumber, previousChapter, siblingType, siblingName, currentTime
+    title, chapterNumber, previousChapter, siblingType, siblingName, currentTime
 }: {
-   title: string,
-   chapterNumber: number,
-   previousChapter: string,
-   siblingType: string,
-   siblingName: string,
-   currentTime: string
+    title: string,
+    chapterNumber: number,
+    previousChapter: string,
+    siblingType: string,
+    siblingName: string,
+    currentTime: string
 }) => `
    You are a professional storyteller.
    Create a chapter for a story with a title of "${title}", and based of the previous chapter below.
@@ -37,39 +37,46 @@ const nthChapterPrompt = ({
    ${previousChapter}`;
 
 export async function POST(request: Request) {
-   const {
-      title,
-      chapterNumber,
-      previousChapter,
-      siblingType,
-      siblingName,
-      currentTime
-   } = await request.json();
+    let requestJSON;
+    try {
+        requestJSON = await request.json();
+    } catch (error) {
+        return NextResponse.json({error: "Invalid request JSON"});
+    }
 
-   if (!title || !chapterNumber || !previousChapter || !siblingType || !siblingName || !currentTime) {
-      return NextResponse.json({error: "title, chapterNumber, previousChapter, siblingType, siblingName, and currentTime are required"})
-   }
+    const {
+        title,
+        chapterNumber,
+        previousChapter,
+        siblingType,
+        siblingName,
+        currentTime
+    } = requestJSON;
 
-   let completion = await openai.chat.completions.create({
-      messages: [{
-         role: "system",
-         content: nthChapterPrompt({
-            title,
-            chapterNumber,
-            previousChapter,
-            siblingType,
-            siblingName,
-            currentTime
-         })
-      }],
-      // model: "gpt-3.5-turbo",
-      // model: "gpt-4-1106-preview",
-      model: "gpt-4",
-   });
+    if (!title || !chapterNumber || !previousChapter || !siblingType || !siblingName || !currentTime) {
+        return NextResponse.json({error: "title, chapterNumber, previousChapter, siblingType, siblingName, and currentTime are required"})
+    }
 
-   const newChapter = completion.choices[0]?.message?.content || "";
+    let completion = await openai.chat.completions.create({
+        messages: [{
+            role: "system",
+            content: nthChapterPrompt({
+                title,
+                chapterNumber,
+                previousChapter,
+                siblingType,
+                siblingName,
+                currentTime
+            })
+        }],
+        // model: "gpt-3.5-turbo",
+        // model: "gpt-4-1106-preview",
+        model: "gpt-4",
+    });
 
-   return NextResponse.json({
-      content: newChapter
-   });
+    const newChapter = completion.choices[0]?.message?.content || "";
+
+    return NextResponse.json({
+        content: newChapter
+    });
 }
