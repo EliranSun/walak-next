f7d27bff46961709562bedc1839450c2001fc806
Eliran Shemesh <piro27@gmail.com>
2023-12-10 13:21:27 +0200

    Do include generic time frame for each chapter

--- app/story/chapter/prompts.ts
+++ app/story/chapter/prompts.ts
@@ -13,13 +13,18 @@ export const firstChapterPrompt = ({
    genre: string,
    currentTime: string
 }) => {
+   const timeHour = currentTime.split(":")[0];
+   const isMorning = Number(timeHour) < 12 && Number(timeHour) > 6;
    const timeMessage = `In addition include the current time - ${currentTime} - somehow in the chapter.`;
+
    return `
    You are a professional storyteller.
    Create a short first chapter for a story with a title of "${title}".
    This is chapter 1 out of 7.
    The chapter will include me, Eliran, and my ${siblingType} ${siblingName}.
    The chapter should encapsulate the feeling of being ${feeling} and the genre should be ${genre}.
+   
+   The chapter should take place in the ${isMorning ? "morning" : "night"}.
 
    Most importantly - at the end of the chapter present two options for the reader to choose from. 
    These choices will affect the rest of the story. Return these options in the following format:
@@ -51,6 +56,8 @@ export const nthChapterPrompt = ({
    genre: string,
    chosenOption: number
 }) => {
+   const timeHour = currentTime.split(":")[0];
+   const isMorning = Number(timeHour) < 12 && Number(timeHour) > 6;
    let storyExtraDirection = "";
    switch (chapterNumber) {
       case 5:
@@ -81,6 +88,9 @@ export const nthChapterPrompt = ({
    choice made by the reader. The reader chose option number ${chosenOption}. The choice is out of two options from the 
    previous chapter, written below. Based on the given choice, continue the story and write this current chapter.
    
+   The chapter should take place in the ${isMorning ? "morning of the following day." : "night of the same day"} and
+   the events occurring in this chapter should make sense based on the time passed since the previous chapter.   
+
     This chapter should include me, Eliran, and my ${siblingType} ${siblingName}.
     The chapter should encapsulate the feeling of being ${feeling} and the genre should be ${genre}.
     This is chapter number ${chapterNumber} out of 7.
