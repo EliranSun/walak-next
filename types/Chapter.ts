39610ffc265c92c85339d1eb104131b6ec176801
Eliran Shemesh <piro27@gmail.com>
2024-01-21 17:13:40 +0200

    Remove all stories-dashboard related components

--- types/Chapter.ts
+++
@@ -1,9 +0,0 @@
-export type Chapter = {
-    title: string;
-    content: string;
-    sibling: string;
-    translation: string;
-    id: number;
-    chapterNumber: number;
-    chapter_number?: number;
-}
