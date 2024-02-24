460eebae69cf3229bc23ffdeea84822141dae206
Eliran Shemesh <piro27@gmail.com>
2024-02-24 18:15:17 +0200

    Don't use next-intl

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -12,59 +12,59 @@ import {getPost} from "@/utils/posts";
 // };
 
 export default async function RootLayout({
-   children,
-   params,
+    children,
+    params,
 }: {
-   children: React.ReactNode,
-   params: { postId: string, postName: string },
+    children: React.ReactNode,
+    params: { postId: string, postName: string },
 }) {
-   let messages;
-   const locale = useLocale();
-   try {
-      messages = (await import(`../../messages/${locale}.json`)).default;
-   } catch (error) {
-      console.log("error", error);
-      notFound();
-   }
+    let messages;
+    // const locale = useLocale();
+    try {
+        // messages = (await import(`../../messages/${locale}.json`)).default;
+    } catch (error) {
+        console.log("error", error);
+        notFound();
+    }
 
-   console.log({params})
-   // const {post, error} = await getPost(Number(params.postId));
+    console.log({params})
+    // const {post, error} = await getPost(Number(params.postId));
 
 
-   return (
-      <html lang={locale}>
-      <head>
-         {/*<title>וואלק | {post.title}</title>*/}
-         {/*<meta name="theme-color" content="#f1f5f9"/>*/}
-         {/*<meta property="description" content={post.excerpt}/>*/}
-         {/*<meta property="og:title" content={`וואלק | ${post.title}`}/>*/}
-         {/*<meta property="og:description" content={post.excerpt}/>*/}
-         {/*<meta property="og:image" content={post.metadataImageSrc || post.imageSrc}/>*/}
-         {/*<meta property="og:image:width" content="1200"/>*/}
-         {/*<meta property="og:image:height" content="666"/>*/}
-         {/*<meta property="og:image:type" content="image/jpeg"/>*/}
-         {/*<meta property="og:url" content={`https://walak-next.vercel.app/he/posts/${post.id}/the-swiss-watch`}/>*/}
-         {/*<meta property="og:site_name" content="וואלק"/>*/}
-         {/*<meta property="og:locale" content="he_IL"/>*/}
-         {/*<meta property="og:type" content="article"/>*/}
-         {/*<meta property="twitter:site" content="@Walak"/>*/}
-         {/*<meta property="twitter:creator" content="@Walak"/>*/}
-         {/*<meta property="twitter:card" content="summary_large_image"/>*/}
-         {/*<meta name="twitter:image" content={post.metadataImageSrc || post.imageSrc}/>*/}
-         {/*<meta name="twitter:label1" content="Written by"/>*/}
-         {/*<meta name="twitter:data1" content="Eliran Shemesh & Ofir Cohen"/>*/}
-         {/*<meta name="twitter:label2" content="Est. reading time"/>*/}
-         {/*<meta name="twitter:data2" content="3 minutes"/>*/}
-      </head>
-      <body>
-      <main>
-         {/*<NextIntlClientProvider locale={locale} messages={messages}>*/}
+    return (
+        <html lang="en">
+        <head>
+            {/*<title>וואלק | {post.title}</title>*/}
+            {/*<meta name="theme-color" content="#f1f5f9"/>*/}
+            {/*<meta property="description" content={post.excerpt}/>*/}
+            {/*<meta property="og:title" content={`וואלק | ${post.title}`}/>*/}
+            {/*<meta property="og:description" content={post.excerpt}/>*/}
+            {/*<meta property="og:image" content={post.metadataImageSrc || post.imageSrc}/>*/}
+            {/*<meta property="og:image:width" content="1200"/>*/}
+            {/*<meta property="og:image:height" content="666"/>*/}
+            {/*<meta property="og:image:type" content="image/jpeg"/>*/}
+            {/*<meta property="og:url" content={`https://walak-next.vercel.app/he/posts/${post.id}/the-swiss-watch`}/>*/}
+            {/*<meta property="og:site_name" content="וואלק"/>*/}
+            {/*<meta property="og:locale" content="he_IL"/>*/}
+            {/*<meta property="og:type" content="article"/>*/}
+            {/*<meta property="twitter:site" content="@Walak"/>*/}
+            {/*<meta property="twitter:creator" content="@Walak"/>*/}
+            {/*<meta property="twitter:card" content="summary_large_image"/>*/}
+            {/*<meta name="twitter:image" content={post.metadataImageSrc || post.imageSrc}/>*/}
+            {/*<meta name="twitter:label1" content="Written by"/>*/}
+            {/*<meta name="twitter:data1" content="Eliran Shemesh & Ofir Cohen"/>*/}
+            {/*<meta name="twitter:label2" content="Est. reading time"/>*/}
+            {/*<meta name="twitter:data2" content="3 minutes"/>*/}
+        </head>
+        <body>
+        <main>
+            {/*<NextIntlClientProvider locale={locale} messages={messages}>*/}
             <Header/>
             {children}
             <Footer/>
-         {/*</NextIntlClientProvider>*/}
-      </main>
-      </body>
-      </html>
-   );
+            {/*</NextIntlClientProvider>*/}
+        </main>
+        </body>
+        </html>
+    );
 }
