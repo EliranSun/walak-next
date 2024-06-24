e941086f42e32baf855095e807b74947424b7712
Eliran Shemesh <piro27@gmail.com>
2024-06-24 15:36:30 +0300

    Update API key + model

--- utils/openAI.ts
+++ utils/openAI.ts
@@ -1,12 +1,12 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-    apiKey: "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",
+    apiKey: "sk-proj-OJtDtq5jxMsMOHjYEHbMT3BlbkFJMwh0jsaKRPuCKEskxBnl",
 });
 
 export const chat = async (prompt: string) => {
     const completion = await openai.chat.completions.create({
-        model: "gpt-4",
+        model: "gpt-4o",
         messages: [{
             role: "system",
             content: prompt,
