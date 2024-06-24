fd2c73d2a2ef87c05a9b5a3020a1ff2897456e67
Eliran Shemesh <piro27@gmail.com>
2024-06-24 16:20:15 +0300

    Incorrect async context?

--- app/nutrition/route.ts
+++ app/nutrition/route.ts
@@ -3,12 +3,11 @@ import {getFoodNutritionPrompt} from "@/utils/prompts";
 import {NextRequest, NextResponse} from "next/server";
 
 export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const food = searchParams.get("food");
+
     try {
-        const searchParams = request.nextUrl.searchParams;
-        const food = searchParams.get("food");
-
         console.log({food});
-
         if (!food) {
             return NextResponse.json({
                 error: "Missing required fields."
