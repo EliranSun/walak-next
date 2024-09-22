af46d20e873132eb0bd45ede69985c18c3ea55a0
Eliran Shemesh <piro27@gmail.com>
2024-09-22 13:48:44 +0300

    no cache

--- app/api/rss/route.ts
+++ app/api/rss/route.ts
@@ -1,17 +1,19 @@
-import {NextRequest} from "next/server";
+import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-   const urls = [
-      "https://www.ynet.co.il/Integration/StoryRss1854.xml",
-      "https://www.wired.com/feed/rss",
-      ];
-   
-   const responses = await Promise.all(urls.map(url => fetch(url)));
-   const feeds = await Promise.all(responses.map(res => res.text()));
-   
-   return new Response(feeds.join("\n"), {
-      headers: {
-         "Content-Type": "application/xml"
-      }
-   });
+	const urls = [
+		"https://www.ynet.co.il/Integration/StoryRss1854.xml",
+		"https://www.wired.com/feed/rss",
+	];
+
+	const responses = await Promise.all(
+		urls.map((url) => fetch(url, { cache: "no-store" }))
+	);
+	const feeds = await Promise.all(responses.map((res) => res.text()));
+
+	return new Response(feeds.join("\n"), {
+		headers: {
+			"Content-Type": "application/xml",
+		},
+	});
 }
