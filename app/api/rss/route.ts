7a5809d535594a380de638e2bf6ac14a881c8fb4
Eliran Shemesh <piro27@gmail.com>
2024-09-22 13:25:20 +0300

    multiple feeds

--- app/api/rss/route.ts
+++ app/api/rss/route.ts
@@ -1,12 +1,17 @@
 import {NextRequest} from "next/server";
 
 export async function GET(request: NextRequest) {
-   const URL = "https://www.ynet.co.il/Integration/StoryRss1854.xml";
-   const response = await fetch(URL);
-   const data = await response.text();
-   return new Response(data, {
+   const urls = [
+      "https://www.ynet.co.il/Integration/StoryRss1854.xml",
+      "https://www.wired.com/feed/rss",
+      ];
+   
+   const responses = await Promise.all(urls.map(url => fetch(url)));
+   const feeds = await Promise.all(responses.map(res => res.text()));
+   
+   return new Response(feeds.join("\n"), {
       headers: {
-         "Content-Type": "application/xml",
+         "Content-Type": "application/xml"
       }
    });
 }
