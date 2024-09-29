63e775c014ab2ef30cc9417c47b0a8cbebd5054a
Eliran Shemesh <piro27@gmail.com>
2024-09-29 13:47:21 +0300

    add import

--- app/api/rss/route.ts
+++ app/api/rss/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-// import OpenAI from "openai";
+import OpenAI from "openai";
 import { parseString } from "xml2js";
 import { promisify } from "util";
 
