6dd26ffbe39811702ef65aae097482759af47375
Eliran Shemesh <piro27@gmail.com>
2025-06-12 16:50:59 +0300

    Use key to store data

--- app/api/store-blocks-data/route.js
+++ app/api/store-blocks-data/route.js
@@ -11,8 +11,12 @@ const Headers = {
 };
 
 export async function POST(request) {
-    const data = request?.body;
-    if (!data) {
+    const body = JSON.parse(request?.body);
+
+    const data = body?.data;
+    const key = body?.key;
+
+    if (!data || !key) {
         return new NextResponse("Missing parameters", {
             status: 500,
             headers: Headers
@@ -21,7 +25,7 @@ export async function POST(request) {
 
     let fileUrl;
     try {
-        const {url} = await put(`blocks/${new Date().toISOString()}.json`, data, {access: 'public'});
+        const {url} = await put(`blocks/${key}.json`, data, {access: 'public'});
         fileUrl = url;
     } catch (error) {
         return new NextResponse(error.message, {
