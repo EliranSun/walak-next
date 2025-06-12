1ab9e7818bb14030cf30a8574afa47f797e7c6f6
Eliran Shemesh <piro27@gmail.com>
2025-06-12 16:32:36 +0300

    Put data

--- app/api/blocks/route.js
+++ app/api/blocks/route.js
@@ -4,8 +4,20 @@ import { put } from "@vercel/blob";
 export const maxDuration = 60;
 export const dynamic = "force-dynamic";
 
-export async function POST() {
-    const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
+export async function POST(request) {
+    const data = request?.body?.data;
+    if (!data) {
+        return new NextResponse("Missing parameters", {
+            status: 500,
+            headers: {
+                "Cache-Control": "no-store, max-age=0",
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+            }
+        });
+    }
+
+    const { url } = await put(`blocks/${new Date().toDateString()}.json`, data, { access: 'public' });
 
     return NextResponse.json({
         success: true,
