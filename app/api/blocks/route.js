ce5b535e51f4522c019a0ebfc030cc777588caaa
Eliran Shemesh <piro27@gmail.com>
2025-06-12 16:42:24 +0300

    Log API error

--- app/api/blocks/route.js
+++ app/api/blocks/route.js
@@ -4,30 +4,37 @@ import { put } from "@vercel/blob";
 export const maxDuration = 60;
 export const dynamic = "force-dynamic";
 
+const Headers = {
+    "Cache-Control": "no-store, max-age=0",
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "application/json",
+};
+
 export async function POST(request) {
     const data = request?.body;
     if (!data) {
         return new NextResponse("Missing parameters", {
             status: 500,
-            headers: {
-                "Cache-Control": "no-store, max-age=0",
-                "Access-Control-Allow-Origin": "*",
-                "Content-Type": "application/json",
-            }
+            headers: Headers
         });
     }
 
-    const { url } = await put(`blocks/${new Date().toDateString()}.json`, data, { access: 'public' });
+    let fileUrl;
+    try {
+        const {url} = await put(`blocks/${new Date().toDateString()}.json`, data, {access: 'public'});
+        fileUrl = url;
+    } catch (error) {
+        return new NextResponse(error.message, {
+            status: 500,
+            headers: Headers
+        });
+    }
 
     return NextResponse.json({
         success: true,
-        url,
-        data
+        url: fileUrl,
+        data,
     }, {
-        headers: {
-            "Cache-Control": "no-store, max-age=0",
-            "Access-Control-Allow-Origin": "*",
-            "Content-Type": "application/json",
-        },
+        headers: Headers
     });
 }
